f63bbf6d2e6e4f1ee7392096496ac25976bf97bf
Elias Hasle <elias.hasle@gmail.com>
2019-07-25 12:41:42 +0200

    NPM package

---
+++ index.js
@@ -0,0 +1,3 @@
+export {Samples} from "./src/Samples.js";
+export {Patches} from "./src/Patches.js";
+export {DynamicPatches} from "./src/DynamicPatches.js";
---
+++ rollup.config.js
@@ -0,0 +1,8 @@
+export default {
+	input: "./map-view-controls.js",
+	output: {
+		file: "build/MapViewControls.js",
+		format: "umd",
+		name: "map-view-controls"
+	}
+};
