c4af38dabb02f2dda4233284cc7dd1efe0575414
Elias Hasle <elias.hasle@gmail.com>
2019-08-14 12:05:18 +0200

    Cleanup

--- build/MapViewControls.js
+++ build/MapViewControls.js
@@ -6,19 +6,14 @@
 
 	//By Elias Hasle. Adapted from a vessel.js demo (also by Elias Hasle)
 
-	//import {Vector2} from "three/src/math/Vector2.js";
-	//const _v = new Vector2();
-
 	//The argument is a THREE.OrthographicCamera
-	function MapViewControls(oCamera, domElement, /*renderer, */preset) {
+	function MapViewControls(oCamera, domElement, preset) {
 		Object.assign(this, {
 			HMAX: 600,
 			minX: -800,
 			maxX: 800,
 			minY: -600,
 			maxY: 600,
-			//offsetX: 0,
-			//offsetY: 0,
 		}, preset || {});
 		
 		this.mouseX = 0;
@@ -27,16 +22,10 @@
 		
 		this.oCamera = oCamera;
 		this.domElement = domElement;
-		//this.renderer = renderer;
 		
-		// oCamera.position.set(0,0,this.HMAX);
-		oCamera.position.z = this.HMAX;
+		oCamera.position.z = Math.max(oCamera.position.z, this.HMAX);
 		
 		let scope = this;
-		
-		/*this.updateFun = function(e) {
-			scope.update.call(scope);
-		}*/
 
 		//Read mouse wheel input:	
 		this.onWheel = function(e) {
@@ -54,14 +43,12 @@
 	Object.assign(MapViewControls.prototype, {
 		constructor: MapViewControls,
 		//This function updates the orthographic camera
-							//optional parameters
-							//wheel is wheel rotation direction, -1,0 or 1.
+		//wheel is wheel rotation direction, -1,0 or 1.
 		update: function(wheel=0) {
-			if (wheel === 0) return;
+			if (!wheel) return;
 			
 			let width = this.domElement.clientWidth;
 			let height = this.domElement.clientHeight;
-			//this.renderer.setSize(width,height);
 			
 			let aspect = width/height;
 			
@@ -82,27 +69,21 @@
 			//Configures camera
 			let maxWidth = (this.maxX-this.minX);
 			Object.assign(this.oCamera, {
-				// left: this.offsetX,
 				left: -0.5*maxWidth*this.scale,
-				// right: maxWidth*this.scale +this.offsetX,
 				right: 0.5*maxWidth*this.scale,
-				// top: (0.5*maxWidth/aspect)*this.scale +this.offsetY,
 				top: (0.5*maxWidth/aspect)*this.scale,
-				// bottom: (-0.5*maxWidth/aspect)*this.scale +this.offsetY
 				bottom: (-0.5*maxWidth/aspect)*this.scale
 			});
 			
 			this.oCamera.updateProjectionMatrix();
 		},
 		enable() {
-			//window.addEventListener("resize", this.updateFun);
 			this.domElement.addEventListener("wheel", this.onWheel);
 			// this.domElement.addEventListener("mousedown", this.onMousedown);
 			// this.domElement.addEventListener("mouseup", this.onMouseup);
 			this.update();
 		},
 		disable() {
-			//window.removeEventListener("resize", this.updateFun);
 			this.domElement.removeEventListener("wheel", this.onWheel);
 			// this.domElement.removeEventListener("mousedown", this.onMousedown);
 			// this.domElement.removeEventListener("mouseup", this.onMouseup);
--- map-view-controls.js
+++ map-view-controls.js
@@ -1,18 +1,13 @@
 //By Elias Hasle. Adapted from a vessel.js demo (also by Elias Hasle)
 
-//import {Vector2} from "three/src/math/Vector2.js";
-//const _v = new Vector2();
-
 //The argument is a THREE.OrthographicCamera
-function MapViewControls(oCamera, domElement, /*renderer, */preset) {
+function MapViewControls(oCamera, domElement, preset) {
 	Object.assign(this, {
 		HMAX: 600,
 		minX: -800,
 		maxX: 800,
 		minY: -600,
 		maxY: 600,
-		//offsetX: 0,
-		//offsetY: 0,
 	}, preset || {});
 	
 	this.mouseX = 0;
@@ -21,16 +16,10 @@ function MapViewControls(oCamera, domElement, /*renderer, */preset) {
 	
 	this.oCamera = oCamera;
 	this.domElement = domElement;
-	//this.renderer = renderer;
 	
-	// oCamera.position.set(0,0,this.HMAX);
-	oCamera.position.z = this.HMAX;
+	oCamera.position.z = Math.max(oCamera.position.z, this.HMAX);
 	
 	let scope = this;
-	
-	/*this.updateFun = function(e) {
-		scope.update.call(scope);
-	}*/
 
 	//Read mouse wheel input:	
 	this.onWheel = function(e) {
@@ -48,14 +37,12 @@ MapViewControls.prototype = Object.create(Object.prototype);
 Object.assign(MapViewControls.prototype, {
 	constructor: MapViewControls,
 	//This function updates the orthographic camera
-						//optional parameters
-						//wheel is wheel rotation direction, -1,0 or 1.
+	//wheel is wheel rotation direction, -1,0 or 1.
 	update: function(wheel=0) {
-		if (wheel === 0) return;
+		if (!wheel) return;
 		
 		let width = this.domElement.clientWidth;
 		let height = this.domElement.clientHeight;
-		//this.renderer.setSize(width,height);
 		
 		let aspect = width/height;
 		
@@ -76,27 +63,21 @@ Object.assign(MapViewControls.prototype, {
 		//Configures camera
 		let maxWidth = (this.maxX-this.minX);
 		Object.assign(this.oCamera, {
-			// left: this.offsetX,
 			left: -0.5*maxWidth*this.scale,
-			// right: maxWidth*this.scale +this.offsetX,
 			right: 0.5*maxWidth*this.scale,
-			// top: (0.5*maxWidth/aspect)*this.scale +this.offsetY,
 			top: (0.5*maxWidth/aspect)*this.scale,
-			// bottom: (-0.5*maxWidth/aspect)*this.scale +this.offsetY
 			bottom: (-0.5*maxWidth/aspect)*this.scale
 		});
 		
 		this.oCamera.updateProjectionMatrix();
 	},
 	enable() {
-		//window.addEventListener("resize", this.updateFun);
 		this.domElement.addEventListener("wheel", this.onWheel);
 		// this.domElement.addEventListener("mousedown", this.onMousedown);
 		// this.domElement.addEventListener("mouseup", this.onMouseup);
 		this.update();
 	},
 	disable() {
-		//window.removeEventListener("resize", this.updateFun);
 		this.domElement.removeEventListener("wheel", this.onWheel);
 		// this.domElement.removeEventListener("mousedown", this.onMousedown);
 		// this.domElement.removeEventListener("mouseup", this.onMouseup);
